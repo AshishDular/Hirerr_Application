7ff9bbbefcebc987e37838669f06ac95da3ca022
Ashish Dular <119157107+AshishDular@users.noreply.github.com>
2023-05-22 11:45:09 +0530

    Add featured and trustedBy components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+    </div>
+  );
 }
 
 export default Home;
