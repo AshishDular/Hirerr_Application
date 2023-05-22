59f47911b9820fb8350af2d03c8b3ac863e02ed3
Ashish Dular <119157107+AshishDular@users.noreply.github.com>
2023-05-22 17:55:46 +0530

    Add gigCard and projectCard component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import Featured from "../../components/featured/Featured";
 import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 function Home() {
   return (
@@ -13,6 +14,91 @@ function Home() {
       <Slide slidesToShow={5} arrowsScroll={5}>
         {cards.map((card) => (
           <CatCard key={card.id} item={card} />
+        ))}
+      </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <hr />
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>hirerr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>
+              Upgrade to a curated experience to access vetted talent and
+              exclusive tools
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Talent matching
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Dedicated account management
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Team collaboration tools
+            </div>
+            <button>Explore Liverr Business</button>
+          </div>
+
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} item={card} />
         ))}
       </Slide>
     </div>
