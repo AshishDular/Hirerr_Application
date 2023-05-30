dd2e40c2f7b317838371d2aaeee75762c465edb4
Ashish Dular <119157107+AshishDular@users.noreply.github.com>
2023-05-30 13:45:03 +0530

    Add controllers, middleware, models and routes

---
+++ server/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/server/users", userRoute);
+app.use("/server/gigs", gigRoute);
+app.use("/server/orders", orderRoute);
+app.use("/server/conversations", conversationRoute);
+app.use("/server/messages", messageRoute);
+app.use("/server/reviews", reviewRoute);
+app.use("/server/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("running");
+});
